54b2d1f239ad2a0ff47149fd3064d1ee2f544fff
Thomas Jeanneau <thomas@empathie.io>
2019-03-26 21:29:24 +0100

    fix functions

--- functions/fetch.js
+++ functions/fetch.js
@@ -1,11 +1,6 @@
-import fetch from "node-fetch";
-
-const API_ENDPOINT =
-  "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
-
-  exports.handler = (event, context, callback) => {
-    callback(null, {
-      statusCode: 200,
-      body: 'No worries, all is working fine!'
-    })
-  }
+exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: 'No worries, all is working fine!'
+  })
+}
